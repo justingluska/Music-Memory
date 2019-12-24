10b42710b4515594c66c00bd1be6edaa6230de25
Justin Gluska <justingluska@gmail.com>
2019-12-23 19:28:09 -0500

    errors

--- Music Memory/ViewController.swift
+++ Music Memory/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import MediaPlayer
+import StoreKit
 
+
+@objcMembers
 class ViewController: UIViewController {
 
+    /// The instance of `AuthorizationManager` used for querying and requesting authorization status.
+    var authorizationManager: AuthorizationManager!
+    
+    /// The instance of `AuthorizationDataSource` that provides information for the `UITableView`.
+    var authorizationDataSource: AuthorizationDataSource!
+    
+    /// A boolean value representing if a `SKCloudServiceSetupViewController` was presented while the application was running.
+    var didPresentCloudServiceSetup = false
+
+    @IBAction func authButton(_ sender: Any) {
+        authorizationManager.requestCloudServiceAuthorization()
+        
+        authorizationManager.requestMediaLibraryAuthorization()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let controller = SKCloudServiceController()
+
+        controller.requestStorefrontCountryCode { countryCode, error in
+            // Use the value in countryCode for subsequent API requests
+        }
+
+        
     }
 
 
 }
 
+func requestAuthorization(_ handler: @escaping (MPMediaLibraryAuthorizationStatus) -> Void){
+}
