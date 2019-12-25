d0b8e6cf6b08c57d7b2408e08f7ae2372f1f1774
Justin Gluska <justingluska@gmail.com>
2019-12-25 12:42:29 -0500

    Song dates WORKING

--- Music Memory/ViewController.swift
+++ Music Memory/ViewController.swift
@@ -9,40 +9,72 @@
 import UIKit
 import MediaPlayer
 import StoreKit
+import Foundation
 
 
-@objcMembers
+struct OverviewData {
+    var topGenre: String
+    var topArtist: String
+    var topSong: String
+    var totalPlays: Int
+    var totalTime: TimeInterval
+    var numTracks: Int
+    var avgDuration: TimeInterval
+    var avgPlays: Float
+    var avgSkips: Float
+    var explicitRatio: Float
+    //var dateAdded: Date
+}
+
 class ViewController: UIViewController {
 
-    /// The instance of `AuthorizationManager` used for querying and requesting authorization status.
-    var authorizationManager: AuthorizationManager!
-    
-    /// The instance of `AuthorizationDataSource` that provides information for the `UITableView`.
-    var authorizationDataSource: AuthorizationDataSource!
-    
-    /// A boolean value representing if a `SKCloudServiceSetupViewController` was presented while the application was running.
-    var didPresentCloudServiceSetup = false
-
-    @IBAction func authButton(_ sender: Any) {
-        authorizationManager.requestCloudServiceAuthorization()
-        
-        authorizationManager.requestMediaLibraryAuthorization()
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let controller = SKCloudServiceController()
-
-        controller.requestStorefrontCountryCode { countryCode, error in
-            // Use the value in countryCode for subsequent API requests
-        }
-
+        fetchOverview()
         
     }
 
-
+    
+    @IBOutlet weak var label: UITextView!
+    
+    
+   func fetchOverview() -> OverviewData? {
+    guard let songs = MPMediaQuery.songs().items else {
+        return nil
+    }
+    
+    var topGenre: (String, Int) = ("None", -1)
+    var topArtist = ("None", -1)
+    var topSong = ("None", -1)
+    var totalPlays: Int = 0
+    var totalTime: TimeInterval = 0
+    var totalDiscSize: Int = 0
+    var totalDurations: TimeInterval = 0
+    var totalExplicit: Int = 0
+    var totalSkips: Int = 0
+    
+    
+    var all:String = ""
+    
+    for song in songs{
+        all = all + ("\n\(song.title!)->\(song.dateAdded)")
+        label.text = all
+    }
+    
+    return OverviewData(
+        topGenre: topGenre.0,
+        topArtist: topArtist.0,
+        topSong: topSong.0,
+        totalPlays: totalPlays,
+        totalTime: totalTime,
+        numTracks: songs.count,
+        avgDuration: totalDurations / Double(songs.count),
+        avgPlays: Float(totalPlays) / Float(songs.count),
+        avgSkips: Float(totalSkips) / Float(songs.count),
+        explicitRatio: Float(totalExplicit) / Float(songs.count)
+    )
+    }
+    
 }
 
-func requestAuthorization(_ handler: @escaping (MPMediaLibraryAuthorizationStatus) -> Void){
-}
