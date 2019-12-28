b507b0a8ea821339cfba7409bb41c4eaaaaa30c3
Justin Gluska <justingluska@gmail.com>
2019-12-27 22:36:32 -0500

    before something big

--- Music Memory/InterestsViewController.swift
+++ Music Memory/InterestsViewController.swift
@@ -15,13 +15,24 @@ class InterestsViewController: UIViewController
     
     @IBOutlet weak var contentView: UIView!
     var interests = Interest.fetchInterests()
+    let cellScale: CGFloat = 0.6
     
     @IBOutlet weak var songsFromTodayLabel: UILabel!
     override func viewDidLoad(){
         super.viewDidLoad()
         try? VideoBackground.shared.play(view: view, videoName: "stats", videoType: "mp4")
         collectionView.dataSource = self
-        
+        collectionView.layer.cornerRadius = 10
+//        let screenSize = UIScreen.main.bounds.size
+//        let cellWidth = floor(screenSize.width * cellScale)
+//        let cellHeight = floor(screenSize.height * cellScale)
+//        let insetX = (view.bounds.width - cellWidth) / 2.0
+//        let insetY = (view.bounds.width - cellHeight) / 2.0
+//        
+//        let layout = collectionView!.collectionViewLayout as! UICollectionViewFlowLayout
+//        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+//        collectionView.contentInset = UIEdgeInsets(top: insetY, left:insetX, bottom:insetY, right:insetX)
+        collectionView.dataSource = self
     }
 }
 
@@ -43,5 +54,19 @@ extension InterestsViewController: UICollectionViewDataSource
         cell.interest = interest
         
         return cell
+    }
+    
+}
+
+extension InterestsViewController : UIScrollViewDelegate, UICollectionViewDelegate
+{
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let layout = self.collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: scrollView.contentInset.top)
+        targetContentOffset.pointee = offset
     }
 }
